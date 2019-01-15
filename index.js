8c6eeb2247a46a18fc7f58739a7fda045fb01cf3
DeemoHarlos <cces4415@gmail.com>
2019-01-16 05:04:26 +0800

    Add menu and YouTube iframe

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
+$('#content #video>.close').$e('click',e=>{$('#content #video').style.display = 'none'})
+$('#menu').$e('click',e=>{$('#content #video').style.display = 'none'})
+
 videos.forEach((e,i,a)=>{
-/*	if(i>8) return
-	var iframe = $n('iframe')
-	iframe.attr('width','50%')
-	iframe.attr('height','400')
-	iframe.attr('src',`https://www.youtube.com/embed/${e}`)
-	iframe.attr('frameborder','0')
-	iframe.attr('allow','accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture')
-	iframe.attr('allowfullscreen','')
-	$('#content').apnd(iframe)*/
-	var div = $n('a',`preview${e}`,'preview')
+	var div = $n('div',`preview${e}`,'preview')
 	div.style.background = `url('https://i.ytimg.com/vi/${e}/hqdefault.jpg') center/cover no-repeat`
+	div.$e('click',ele=>{
+		$('#content #video>iframe').attr('src',`https://www.youtube.com/embed/${e}`)
+		$('#content #video').style.display = 'block'
+	})
 	div.attr('href',`https://www.youtube.com/watch?v=${e}`)
 	div.attr('target','_blank')
-	$('#content').apnd(div)
+	$('#content #context').apnd(div)
 })
